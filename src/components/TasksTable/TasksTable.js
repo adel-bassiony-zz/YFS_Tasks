59913c2cc4522a30ddc48984f206910ad2609933
Adel Bassiony <a.bassiony@icloud.com>
2020-11-26 16:55:49 +0200

    Added: Table Filters

--- src/components/TasksTable/TasksTable.js
+++ src/components/TasksTable/TasksTable.js
@@ -1,33 +1,67 @@
-import TasksTableRow from 'components/TasksTableRow/TasksTableRow';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Loader from 'components/Loader/Loader';
+import TasksTableRow from 'components/TasksTableRow/TasksTableRow';
+import './TasksTable.scss';
 
 export default function TasksTable() {
 
     const tasks = useSelector(state => state.tasks_reducer.tasks)
 
-    const tasksRows = tasks.map((task, index) => 
-        <TasksTableRow
-            key={`task-${index}`}
-            index={index}
-            task={task}
-        />
-    )
+    const [status, setStatus] = useState("")
+    const [date, setDate] = useState(new Date())
+
+    const tasksRows = tasks
+        .filter(task => status === "" ? task : task.task_status === status || task.date === date)
+        .map((task, index) => 
+            <TasksTableRow
+                key={`task-${index}`}
+                index={index}
+                task={task}
+            />
+        )
 
     if (tasks.length > 0) {
         return (
             <>
-                <table className="w-100">
-                    <thead>
+                <div className="row mt-4">
+                    <div className="col-md-6">
+                        <h1>Tasks</h1>
+                    </div>
+                    <div className="col-md-3">
+                        <select
+                            className="custom-select"
+                            value={status}
+                            onChange={(e) => setStatus(e.target.value)} >
+                            <option value="" disabled>Choose The Status</option>
+                            <option value="all">All</option>
+                            <option value="0">Assigned</option>
+                            <option value="1">Started</option>
+                            <option value="2">Successful</option>
+                            <option value="3">Failed</option>
+                            <option value="4">InProgress</option>
+                            <option value="6">Unassigned</option>
+                            <option value="7">Accepted</option>
+                            <option value="8">Decline</option>
+                            <option value="9">Cancel</option>
+                            <option value="10">Deleted</option>
+                        </select>
+
+                    </div>
+                    <div className="col-md-3">
+                        <input type="date" placeholder="Enter the date" className="form-control" onChange={(e) => setDate(e.target.value)} />
+                    </div>
+                </div>
+                <table className="w-100 mt-4">
+                    <thead className="table__head">
                         <tr>
-                            <th>ID</th>
-                            <th>Customer Name</th>
-                            <th>Customer Phone</th>
-                            <th>Customer Email</th>
-                            <th>Status</th>
-                            <th>Created At</th>
-                            <th>Actions</th>
+                            <th className="p-2">ID</th>
+                            <th className="p-2">Customer Name</th>
+                            <th className="p-2">Customer Phone</th>
+                            <th className="p-2">Customer Email</th>
+                            <th className="p-2">Status</th>
+                            <th className="p-2">Created At</th>
+                            <th className="p-2">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
--- src/components/TasksTableRow/TasksTableRow.js
+++ src/components/TasksTableRow/TasksTableRow.js
@@ -4,16 +4,54 @@ import './TasksTableRow.scss';
 import ViewTask from 'components/ViewTask/ViewTask';
 import EditTask from 'components/EditTask/EditTask';
 import DeleteTask from 'components/DeleteTask/DeleteTask';
-import useTaskStatus from 'hooks/useTaskStatus';
 
 export default function TasksTableRow(props) {
+
+    // Get Status By ID
+    const getStatusByID = () => {
+        switch (props.task.task_status) {
+            case "0":
+                return "Assigned"
+
+            case "1":
+                return "Started"
+
+            case "2":
+                return "Successful"
+
+            case "3":
+                return "Failed"
+
+            case "4":
+                return "InProgress"
+
+            case "6":
+                return "Unassigned"
+
+            case "7":
+                return "Accepted"
+
+            case "8":
+                return "Decline"
+
+            case "9":
+                return "Cancel"
+
+            case "10":
+                return "Deleted"
+
+            default:
+                return "-"
+        }
+    }
+
     return (
         <tr className="task">
             <td className="task__row">{props.task.task_id}</td>
             <td className="task__row">{props.task.customer_name}</td>
             <td className="task__row">{props.task.customer_phone}</td>
             <td className="task__row">{props.task.customer_email}</td>
-            <td className="task__row">{useTaskStatus(props.task.task_status)}</td>
+            <td className="task__row">{getStatusByID()}</td>
             <td className="task__row">{moment(props.task.task_datetime).format('DD-MM-YYYY hh:mm a')}</td>
             <td className="task__row">
                 <ViewTask task={props.task} />
