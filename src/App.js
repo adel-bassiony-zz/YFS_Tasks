03315460ecda4c264f81d0ec99e657e71d4aaf9b
Adel Bassiony <a.bassiony@icloud.com>
2020-11-26 17:21:09 +0200

    Added: Suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, {Suspense} from 'react';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import TasksTable from 'components/TasksTable/TasksTable';
+import Loader from 'components/Loader/Loader';
 
 function App() {
     return (
         <ErrorBoundary>
-            <div className="px-5">
-                <TasksTable />
-            </div>
+            <Suspense fallback={<Loader/>}>
+                <div className="px-5">
+                    <TasksTable />
+                </div>
+            </Suspense>
         </ErrorBoundary>
     );
 }
